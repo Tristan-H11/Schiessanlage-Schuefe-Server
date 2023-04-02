d8a684e317fbb543983dd017e93e8adebd04371c
Tristan-H11 <triddel02@gmx.de>
2023-04-02 18:05:39 +0200

    filehandling verbessert

--- src/main/java/me/tristan/YamlHandler.java
+++ src/main/java/me/tristan/YamlHandler.java
@@ -3,37 +3,48 @@ package me.tristan;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class YamlHandler {
 
     private final static Yaml yaml = new Yaml();
-    private static Map<String, Integer> counters;
-    private final static String pathname = "./counter.yaml";
+    private static Map<String, Integer> counters = new HashMap<>();
+    private final static Path path = Paths.get("./counter.yaml");
+
 
     public YamlHandler() {
     }
 
     public static void initialize() {
-        try (FileInputStream fileInputStream = new FileInputStream(Paths.get(pathname).toFile())) {
-            File yourFile = new File(pathname);
-            yourFile.createNewFile();
-            counters = yaml.load(fileInputStream);
-        } catch (Exception e) {
+        try {
+            File file = new File(path.toUri());
+            if (!file.exists()) {
+                file.createNewFile();
+                Files.write(path, Collections.singletonList("{a: 0, b: 0, c: 0}"));
+            }
+            try (FileInputStream fileInputStream = new FileInputStream(path.toFile())) {
+                counters = yaml.load(fileInputStream);
+                if (counters == null) {
+                    counters = new HashMap<>();
+                    counters.put("a", 0);
+                    counters.put("b", 0);
+                    counters.put("c", 0);
+                    write();
+                }
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-        if (counters.keySet().size() != 3) {
-            counters.clear();
-            counters.put("a", 0);
-            counters.put("b", 0);
-            counters.put("c", 0);
         }
         System.out.println(counters);
     }
 
     private static void write() {
-        try (FileWriter writer = new FileWriter(Paths.get(pathname).toFile())) {
+        try (FileWriter writer = new FileWriter(path.toFile())) {
             yaml.dump(counters, writer);
         } catch (IOException e) {
             throw new RuntimeException(e);
